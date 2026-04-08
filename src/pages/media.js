b6c79802464f27149055c387d72ecbc05fca46c3
skmoreland <sarah@eustaceconsulting.com>
2026-04-08 13:33:19 -0400

    Adding referrer policy for Youtube videos

--- src/pages/media.js
+++ src/pages/media.js
@@ -33,7 +33,7 @@ const recordings = () => (
     <div className="recordings section boxed">
     {data.allContentfulMedia.edges.map(({ node }) =>(
           <div className="recording">
-            <iframe title={node.title} width="600" height="400" src={node.link}></iframe>
+            <iframe title={node.title} width="600" height="400" referrerPolicy="strict-origin-when-cross-origin" src={node.link}></iframe>
             <br/><br/>
             <h3>{node.title}</h3>
             <br />
